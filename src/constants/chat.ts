7297525f75cf82fce4a17baf19c8b02cdf48ba73
Dorin Chitu <dorin.chitu@esolutions.ro>
2024-03-25 09:48:00 +0200

    as

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -25,7 +25,15 @@ export const modelOptions: ModelOptions[] = [
   'gpt-4',
   'gpt-4-32k',
   'gpt-4-1106-preview',
-  'gpt-4-0125-preview'
+  'gpt-4-0125-preview',
+  "@cf/meta/llama-2-7b-chat-fp16",
+  "@cf/meta/llama-2-7b-chat-int8",
+  "@cf/mistral/mistral-7b-instruct-v0.1",
+  "@hf/thebloke/deepseek-coder-6.7b-instruct-awq",
+  "@cf/deepseek-ai/deepseek-math-7b-instruct",
+  "@cf/tiiuae/falcon-7b-instruct",
+  "@hf/thebloke/llama-2-13b-chat-awq",
+  "@hf/thebloke/mistral-7b-instruct-v0.1-awq"
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
   // 'gpt-4-32k-0314',
@@ -49,6 +57,14 @@ export const modelMaxToken = {
   'gpt-4-32k-0613': 32768,
   'gpt-4-1106-preview': 128000,
   'gpt-4-0125-preview': 128000,
+   "@cf/meta/llama-2-7b-chat-fp16": 8192,
+   "@cf/meta/llama-2-7b-chat-int8": 8192,
+   "@cf/mistral/mistral-7b-instruct-v0.1": 8192,
+   "@hf/thebloke/deepseek-coder-6.7b-instruct-awq": 8192,
+   "@cf/deepseek-ai/deepseek-math-7b-instruct": 8192,
+   "@cf/tiiuae/falcon-7b-instruct": 8192,
+   "@hf/thebloke/llama-2-13b-chat-awq": 8192,
+   "@hf/thebloke/mistral-7b-instruct-v0.1-awq": 8192
 };
 
 export const modelCost = {
